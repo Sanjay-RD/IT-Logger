a6bb73925e0fdd21897fce441a7cc1268100d6d1
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-02-14 17:20:07 +0545

    Search Logs

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { searchLogs } from "../../actions/logActions";
 
-function SearchBar() {
+function SearchBar({ searchLogs }) {
+  const onSearch = (e) => {
+    const text = e.target.value;
+    searchLogs(text);
+  };
   return (
     <nav style={{ marginBottom: "30px" }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Logs ..."
+              onChange={onSearch}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -18,4 +29,4 @@ function SearchBar() {
   );
 }
 
-export default SearchBar;
+export default connect(null, { searchLogs })(SearchBar);
